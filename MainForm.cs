f82b8ef0f5ae665e0302203c19d860a8b7f0e6c5
Kaiser Slocum <slocumkaiser@gmail.com>
2025-07-05 16:34:00 -0500

    Mask Update
    Added ability to have a mask so you can actually add scrabble words to other words per rules.

--- MainForm.cs
+++ MainForm.cs
@@ -11,6 +11,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * TODO:
+ * Have a way to sort my length / points scored
+ * You'll need to create some way to store points per letter
+ */
+
 namespace ScrabbleEngine
 {
     public partial class MainForm : Form
@@ -41,25 +47,47 @@ namespace ScrabbleEngine
             strLetters = new string(cList.ToArray());
         }
 
-        private Boolean WordInLetters(string strWord, string strLetters)
+        private void CreateMask(ref int pIntMinLength, ref bool pBlnIsMask, string pStrMask)
         {
+            for (int i = pStrMask.Length - 1; i >= 0; i--)
+            {
+                pIntMinLength = i + 1;
+
+                char c = pStrMask[i];
+                if (c != '-')
+                {
+                    pBlnIsMask = true;
+                    break;
+                }
+            }
+        }
+
+        private Boolean WordInLetters(string strWord, string strLetters, int intMinLength = 0, bool blnIsMask = false, string strMask = "-------")
+        {
+            if (strWord.Length < intMinLength)
+                return false;
+
             strWord = strWord.ToLower().Trim();
             Boolean invalidChar;
             char[] cLetters = strLetters.ToCharArray();
-            //Console.WriteLine(cLetters[0]);
-            //Console.WriteLine(strWord[0]);
-            
-            foreach (char c in strWord)
+
+            for (int intLetter = 0;  intLetter < strWord.Length; intLetter++)
             {
-                invalidChar = true;
-                for (int i = 0; (i < cLetters.Length) && (invalidChar == true); i++) 
+                char c = strWord[intLetter];
+
+                if (blnIsMask == true)
                 {
-                    if (cLetters[i] == c)
+                    if ((strMask[intLetter] != '-') && (c != strMask[intLetter]))
                     {
-                        cLetters[i] = '-';
-                        invalidChar = false;
+                        return false;
                     }
-                    else if (cLetters[i] == '*')
+                }                    
+
+                invalidChar = true;
+                for (int i = 0; (i < cLetters.Length) && (invalidChar == true); i++)
+                {
+
+                    if ((cLetters[i] == c) || (cLetters[i] == '*'))
                     {
                         cLetters[i] = '-';
                         invalidChar = false;
@@ -81,26 +109,36 @@ namespace ScrabbleEngine
             {
                 Console.WriteLine("Word list file not found.");
                 return;
-            }
-
-            DisplayListBox.Items.Clear();
-
-            int i = 0;
-            ProgressBar.Value = 0;
-            ProgressBar.Maximum = 276645;
+            } 
 
             string strLetters = LettersTextbox.Text;
-            if( strLetters.Length != 7)
+            string strMask = MaskTextBox.Text;
+            if ( strLetters.Length != 7)
             {        
                 MessageBox.Show("Must have seven letters!", "Not 7 letters", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
+            else if (strMask.Length != 7)
+            {
+                MessageBox.Show("Must have seven letters in mask!", "Not 7 letters", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
 
+
+            int intMinLength = 0;
+            bool blnIsMask = false;
+            CreateMask(ref intMinLength, ref blnIsMask, strMask);
+                        
 
             strLetters = strLetters.ToLower().Trim();
             Sort(ref strLetters);
 
             string strTempLetters;
+            ProgressBar.Value = 0;
+            ProgressBar.Maximum = 276645;
+            int intProgressValue = 0;
+
+            DisplayListBox.Items.Clear();
 
             // Read words line by line
             using (StreamReader reader = new StreamReader(filePath))
@@ -109,19 +147,19 @@ namespace ScrabbleEngine
                 while ((word = reader.ReadLine()) != null)
                 {
                     strTempLetters = strLetters;
-                    if (WordInLetters(word, strTempLetters) == true)
+                    if (WordInLetters(word, strTempLetters, intMinLength, blnIsMask, strMask) == true)
                     {
                         //Console.WriteLine(word);
                         DisplayListBox.Items.Add(word);
                     }
 
-                    i++;
-                    ProgressBar.Value = i;
+                    intProgressValue++;
+                    ProgressBar.Value = intProgressValue;
                     //Console.WriteLine(word); // You can replace this with any logic you want                    
                 }
             }
 
-            Console.WriteLine("Done reading all words." + i);
+            Console.WriteLine("Done reading all words." + intProgressValue);
             ProgressBar.Value = 0;
         }
     }
